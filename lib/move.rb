494ca8177a792e1a908ec942bb620f286f58a3c4
Andrew Silva <andrewsilva9@gmail.com>
2019-03-29 01:56:14 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,7 @@ end
 
 def move(turn)
   board = [" ", " ", " "]
-  def update_array_at_with(array, index, value = "X")
+  def update_array_at_with(array, index, value = X)
     array[index] = value
   end
   update_array_at_with(board, 0, "X")
