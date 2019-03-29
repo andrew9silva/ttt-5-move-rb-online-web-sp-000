eda0a31bd134a39bdc1f91277a7188974bf7291b
Andrew Silva <andrewsilva9@gmail.com>
2019-03-29 01:33:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(turn)
+def move(board)
   board = [" ", " ", " "]
   def update_array_at_with(array, index, value)
     array[index] = value
